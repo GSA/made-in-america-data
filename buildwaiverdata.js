3ee7513add0dbb9dac51abb4e46b955be9c6cc56
kedwards-a <kedwards-a@kedwards-l1.fios-router.home>
2022-01-10 09:54:10 -0500

    monday commits

--- buildwaiverdata.js
+++ buildwaiverdata.js
@@ -10,9 +10,11 @@ const {
   FORMS_API_KEY: FORMSKEY,
   CIRCLE_BRANCH,
 } = process.env;
+
 const DATAURL =
   'https://submission.forms.gov/mia-live/madeinamericanonavailabilitywaiverrequest/submission?&select=state,data.piids,data.requestStatus,data.psc,data.procurementTitle,data.contractingOfficeAgencyName,data.waiverCoverage,data.contractingOfficeAgencyId,data.fundingAgencyId,data.fundingAgencyName,data.procurementStage,data.naics,data.summaryOfProcurement,data.waiverRationaleSummary,data.sourcesSoughtOrRfiIssued,data.expectedMaximumDurationOfTheRequestedWaiver,data.isPricePreferenceIncluded,created,modified,data.ombDetermination,data.conditionsApplicableToConsistencyDetermination,data.solicitationId';
 const GITHUBURL = `https://api.github.com/repos/GSA/made-in-america-data/contents/waivers-data.json?ref=${process.env.CIRCLE_BRANCH}`;
+
 async function loadData() {
   try {
     await smokeCheck();
