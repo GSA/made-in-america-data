4a9c8b6d861476db83d939f48303b1a2adb5005f
kedwards-a <kedwards-a@kedwards-l1.fios-router.home>
2022-06-28 15:11:55 -0400

    changed url links for crossover

--- src/Variables/index.js
+++ src/Variables/index.js
@@ -4,8 +4,6 @@ exports.GH_API_KEY = process.env.GH_API_KEY
 exports.FORMS_API_KEY = process.env.FORMS_API_KEY
 exports.URGENT_API_KEY = process.env.URGENT_API_KEY
 exports.BRANCH = process.env.CIRCLE_BRANCH
-exports.DATA_URL =
-  'https://omb.service.forms.gov/mia-live/madeinamericanonavailabilitywaiverrequest/submission?&select=state,data.piids,data.requestStatus,data.psc,data.procurementTitle,data.contractingOfficeAgencyName,data.waiverCoverage,data.contractingOfficeAgencyId,data.fundingAgencyId,data.fundingAgencyName,data.procurementStage,data.naics,data.summaryOfProcurement,data.waiverRationaleSummary,data.sourcesSoughtOrRfiIssued,data.expectedMaximumDurationOfTheRequestedWaiver,data.isPricePreferenceIncluded,created,modified,data.ombDetermination,data.conditionsApplicableToConsistencyDetermination,data.solicitationId,data.countriesOfOriginAndUSContent'
 
 exports.GITHUB_URL = `https://api.github.com/repos/GSA/made-in-america-data/contents/waivers-data.json?ref=${process.env.CIRCLE_BRANCH}`
 
@@ -15,6 +13,8 @@ exports.URGENT_GH_URL = `https://api.github.com/repos/GSA/made-in-america-data/c
 if (process.env.CIRCLE_BRANCH === 'main') {
   exports.URGENT_DATA_URL =
     'https://omb.service.forms.gov/mia-live/urgentrequirementsreport/submission?&select=_id,form,data.waiverType,data.contractingOfficeAgencyId,data.contractingOfficeAgencyName,data.naics,data.psc,data.procurementTitle,data.summaryOfProcurement,data.dateContractSigned,data.identifyUrgencyContributedToNonavailability,data.urgencyContributedExplanation,data.waiverRationaleSummary,data.anticipatedMissionImpactNoWaiver,state,created,modified,data.contractNumber,data.requestStatus,data.ombDetermination,data.conditionsApplicableToConsistencyDetermination'
+  exports.DATA_URL =
+    'https://omb.service.forms.gov/mia-live/madeinamericanonavailabilitywaiverrequest/submission?&select=state,data.piids,data.requestStatus,data.psc,data.procurementTitle,data.contractingOfficeAgencyName,data.waiverCoverage,data.contractingOfficeAgencyId,data.fundingAgencyId,data.fundingAgencyName,data.procurementStage,data.naics,data.summaryOfProcurement,data.waiverRationaleSummary,data.sourcesSoughtOrRfiIssued,data.expectedMaximumDurationOfTheRequestedWaiver,data.isPricePreferenceIncluded,created,modified,data.ombDetermination,data.conditionsApplicableToConsistencyDetermination,data.solicitationId,data.countriesOfOriginAndUSContent'
 } else if (process.env.CIRCLE_BRANCH === 'stage') {
   // STAGE
   exports.URGENT_DATA_URL =
@@ -23,4 +23,6 @@ if (process.env.CIRCLE_BRANCH === 'main') {
   // FORMS DEV URL
   exports.URGENT_DATA_URL =
     'https://test.service.forms.gov/mia-dev/urgentrequirementsreport/submission?&select=_id,form,data.waiverType,data.contractingOfficeAgencyId,data.contractingOfficeAgencyName,data.naics,data.psc,data.procurementTitle,data.summaryOfProcurement,data.dateContractSigned,data.identifyUrgencyContributedToNonavailability,data.urgencyContributedExplanation,data.waiverRationaleSummary,data.anticipatedMissionImpactNoWaiver,state,created,modified,data.contractNumber,data.requestStatus,data.ombDetermination,data.conditionsApplicableToConsistencyDetermination'
+  exports.DATA_URL =
+    'https://test.service.forms.gov/mia-dev/madeinamericanonavailabilitywaiverrequest/submission?&select=state,data.piids,data.requestStatus,data.psc,data.procurementTitle,data.contractingOfficeAgencyName,data.waiverCoverage,data.contractingOfficeAgencyId,data.fundingAgencyId,data.fundingAgencyName,data.procurementStage,data.naics,data.summaryOfProcurement,data.waiverRationaleSummary,data.sourcesSoughtOrRfiIssued,data.expectedMaximumDurationOfTheRequestedWaiver,data.isPricePreferenceIncluded,created,modified,data.ombDetermination,data.conditionsApplicableToConsistencyDetermination,data.solicitationId,data.countriesOfOriginAndUSContent'
 }
