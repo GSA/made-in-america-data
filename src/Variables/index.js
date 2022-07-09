1d3b921d850d976e7a0fde03a93775ea15e4131f
Mesele <42145681+Mesele29@users.noreply.github.com>
2022-07-09 15:45:17 -0400

    rename waivers-data to fix bad data

--- src/Variables/index.js
+++ src/Variables/index.js
@@ -1,4 +1,4 @@
-exports.WAIVER_FILE = './waivers-data.json'
+exports.WAIVER_FILE = './waivers-data-updated.json'
 exports.URGENT_FILE = './urgent-waivers-data.json'
 exports.GH_API_KEY = process.env.GH_API_KEY
 exports.FORMS_API_KEY = process.env.FORMS_API_KEY
@@ -7,7 +7,7 @@ exports.BRANCH = process.env.CIRCLE_BRANCH
 exports.DATA_URL =
   'https://omb.service.forms.gov/mia-live/madeinamericanonavailabilitywaiverrequest/submission?&select=state,data.piids,data.requestStatus,data.psc,data.procurementTitle,data.contractingOfficeAgencyName,data.waiverCoverage,data.contractingOfficeAgencyId,data.fundingAgencyId,data.fundingAgencyName,data.procurementStage,data.naics,data.summaryOfProcurement,data.waiverRationaleSummary,data.sourcesSoughtOrRfiIssued,data.expectedMaximumDurationOfTheRequestedWaiver,data.isPricePreferenceIncluded,created,modified,data.ombDetermination,data.conditionsApplicableToConsistencyDetermination,data.solicitationId,data.countriesOfOriginAndUSContent'
 
-exports.GITHUB_URL = `https://api.github.com/repos/GSA/made-in-america-data/contents/waivers-data.json?ref=${process.env.CIRCLE_BRANCH}`
+exports.GITHUB_URL = `https://api.github.com/repos/GSA/made-in-america-data/contents/waivers-data-updated.json?ref=${process.env.CIRCLE_BRANCH}`
 
 // URGENT WAIVERS URLS
 exports.URGENT_GH_URL = `https://api.github.com/repos/GSA/made-in-america-data/contents/urgent-waivers-data.json?ref=${process.env.CIRCLE_BRANCH}`
